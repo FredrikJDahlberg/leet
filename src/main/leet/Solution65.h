eb2844f1b2a665b9c869b4be3536120be072a054
Fredrik Dahlberg <fredrikjdahlberg@gmail.com>
2024-11-17 20:36:02 +0100

    Added solution 65

---
+++ src/main/leet/Solution65.h
@@ -0,0 +1,17 @@
+//
+// Created by Fredrik Dahlberg on 2024-11-17.
+//
+
+#ifndef SOLUTION65_H
+#define SOLUTION65_H
+
+class Solution65
+{
+public:
+
+    bool isNumber(std::string& s) {
+
+    }
+};
+
+#endif //SOLUTION65_H
